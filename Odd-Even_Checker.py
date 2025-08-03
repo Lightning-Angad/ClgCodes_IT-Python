a4acb27d10a06e828af9aa96656b229567bdccae
Lightning-Angad <ghostcoder.03@gmail.com>
2025-08-03 21:53:28 +0530

    Day_1.1

---
+++ Odd-Even_Checker.py
@@ -0,0 +1,15 @@
+m=int(input("Enter the starting range: "))
+n=int(input("Enter the ending range: "))
+num=m
+print("\nEven Numbers: ")
+while num<=n:
+    if num%2==0:
+        print(num)
+    num += 1
+
+print("\nOdd Numbers: ")
+num=m
+while num<=n:
+    if num%2!=0:
+        print(num)
+    num += 1
