4488e274558e045bc9417ea3491c1fcadbde030a
Lightning-Angad <ghostcoder.03@gmail.com>
2025-08-03 23:05:45 +0530

    Update Leap-Year.py

--- Leap-Year.py
+++ Leap-Year.py
@@ -3,7 +3,7 @@ num = int (input("Enter a year: "))
 if (num%4==0):
  print (num, "is a leap year")
 elif (num%100!=0):
- print (num, "is not a leap year")
+ print (num, "is a leap year")
 elif(num%400==0):
  print(num, "is a leap year")
 else:
