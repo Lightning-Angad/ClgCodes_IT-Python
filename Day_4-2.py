95b0afe12a6360c4a81d1af82c174db5b7c867cc
Lightning-Angad <ghostcoder.03@gmail.com>
2025-09-07 23:15:03 +0530

    Day-4

---
+++ Day_4-2.py
@@ -0,0 +1,7 @@
+set1={0, 1, 2, 3, 4, 5, 6, 7, 99}
+set2={0, 1, 3, 5, 7, 9}
+set3={0, 2, 4, 6, 8}
+print("The union of three sets: ", set1.union(set2, set3))
+print("The intersection of three sets: ", set1.intersection(set2, set3))
+print("The difference of three sets: ", set1.difference(set2, set3))
+print("The symmetric difference of three sets: ", set1.symmetric_difference(set2))
---
+++ Day_4-3.py
@@ -0,0 +1,5 @@
+s1={1, 2, 3, 4, 5}
+s2={2, 4}
+for i in s1:
+    if i not in s2:
+        print(i)
